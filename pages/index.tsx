fb76e4a81fbf8f0c958fbe01b59a16f51f3b5fe4
LandingHeart <shinan.liu@yahoo.com>
2022-07-26 14:53:44 -0400

    svae

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,17 @@ import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
